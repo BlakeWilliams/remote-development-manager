8b95323221f6f0850fe5a50f7ad387aefe0ccb7b
Max Beizer <max.beizer@gmail.com>
2022-04-25 11:09:49 -0500

    Apply suggestions from code review
    Co-authored-by: Blake Williams <blakewilliams@github.com>

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -9,9 +9,9 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "remote-development-manager",
+	Use:   "rdm",
 	Short: "A server and client for better remote development integration.",
-	Long: `Embetter your remove development experience!
+	Long: `Embetter your remote development experience!
 	Complete documentation is available at https://github.com/BlakeWilliams/remote-development-manager`,
 }
 
